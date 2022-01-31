312d11516fc86455934d71ba566947729617ca34
Andres Talavera <andres.talavera@live.fr>
2022-01-31 09:13:57 +0100

    second commit

---
+++ HotelLandonBlog.UI/Controllers/IRazorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelLandonBlog.UI.Controllers
+{
+    public interface IRazorController<T>
+    {
+        Task<ActionResult<IEnumerable<T>>> Index(string? search);
+        Task<ActionResult<T>> Details(int id);
+        Task<ActionResult<T>> Create(int id);
+        Task<ActionResult<T>> Create(int id, T t);
+        Task<ActionResult<T>> Edit(int id);
+        Task<ActionResult<T>> Edit(int id, T t);
+        Task<ActionResult<T>> Delete(int id);
+        Task<ActionResult<T>> Delete();
+    }
+}
