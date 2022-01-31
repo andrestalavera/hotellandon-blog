50227ee98869a7de8d11efe3d6e8f38651fcd72b
mouradelmehdaoui <69789675+mouradelmehdaoui@users.noreply.github.com>
2022-01-31 10:58:33 +0100

    first-update

---
+++ HotelLandonBlog.Repository/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System;
+using HotelLandonBlog.Entities;
+using HotelLandonBlog.Repository;
+
+namespace HotelLandon.Repository
+{
+    public class RepositoryBase<TEntity> : IRepository<TEntity>
+        where TEntity : EntityBase
+    {
+        public Task<TEntity> Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicat = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Undelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Update(int id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
