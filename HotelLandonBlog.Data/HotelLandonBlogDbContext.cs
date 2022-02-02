00187458f474b7ee05cc0a978b1f392c42308245
Andres Talavera <andres.talavera@live.fr>
2022-02-02 15:53:29 +0100

    Reset files

---
+++ HotelLandonBlog.Data/HotelLandonBlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HotelLandonBlog.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelLandonBlog.Data
+{
+    public class HotelLandonBlogDbContext : DbContext, IHotelLandonBlogContext
+    {
+        public DbSet<BlogPost> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public HotelLandonBlogDbContext(DbContextOptions<HotelLandonBlogDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
