6a99ff121754a713acc70585d8928e3161d8fdd2
latifa akkouche <latifaakkouche6@gmail.com>
2022-02-02 18:35:27 +0100

    delete latifa ferhat gay

--- HotelLandonBlog.UI/Controllers/BlogPostsController.Delete.cs
+++ HotelLandonBlog.UI/Controllers/BlogPostsController.Delete.cs
@@ -1,22 +1,36 @@
 ﻿using HotelLandonBlog.Entities;
+using HotelLandonBlog.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
 
 namespace HotelLandonBlog.UI.Controllers
 {
-    public partial class BlogPostsController
+    public partial class BlogPostsController: GenericController<IRepository<BlogPost>, BlogPost>, IRazorController<BlogPost>
     {
+    
         // Get
+        [HttpGet]
         public Task<ActionResult<BlogPost>> Delete(int id)
+        {
+            return view(default(BlogPost));
+        }
+
+        private Task<ActionResult<BlogPost>> view(BlogPost blogPost)
         {
             throw new NotImplementedException();
         }
 
         // Post
-        public Task<ActionResult<BlogPost>> Delete(int id, BlogPost blogPost)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpPost("[action]/{id?}")]
+        public Task<ActionResult<BlogPost>> Delete([Bind(new[] {
+            nameof(BlogPost.Id),
+            nameof(BlogPost.CategoryId),
+            nameof(BlogPost.Category),
+            nameof(BlogPost.Content),
+            nameof(BlogPost.LastUpdate),
+            nameof(BlogPost.Title),
+
+            nameof(BlogPost.Status)})] BlogPost entity, int id) => base.delete(entity, id);
     }
 }
