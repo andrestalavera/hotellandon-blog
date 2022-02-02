ee8a6de1a470edc1f24607ff106291b5fd9fcf0b
Andres Talavera <andres.talavera@live.fr>
2022-02-02 13:40:13 +0100

    Add migrator project

---
+++ HotelLandonBlog.Migrator/Program.cs
@@ -0,0 +1,41 @@
+﻿using HotelLandonBlog.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelLandonBlog.Migrator
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            HotelLandonBlogDbContextFactory factory = new();
+            HotelLandonBlogDbContext context = factory.CreateDbContext();
+
+            await context.Database.EnsureCreatedAsync();
+            await context.Database.MigrateAsync();
+        }
+    }
+
+    public class HotelLandonBlogDbContextFactory : IDesignTimeDbContextFactory<HotelLandonBlogDbContext>
+    {
+        public HotelLandonBlogDbContext CreateDbContext(string[] args = null)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddUserSecrets("HotelLandon-Blog")
+                .Build();
+
+            DbContextOptionsBuilder<HotelLandonBlogDbContext> optionsBuilder = new();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                Console.WriteLine("La chaîne de connexion est vide. Vérifiez votre User-Secret.");
+            }
+            Console.WriteLine(connectionString);
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new HotelLandonBlogDbContext(optionsBuilder.Options);
+        }
+    }
+}
