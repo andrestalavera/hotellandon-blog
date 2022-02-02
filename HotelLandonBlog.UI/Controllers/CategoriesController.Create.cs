f0e1628e6da35e46fd7db758fa1971f99dedd7c2
mouradelmehdaoui <69789675+mouradelmehdaoui@users.noreply.github.com>
2022-02-02 17:22:29 +0100

    View-Controller-Blog-READ-MOURAD

--- HotelLandonBlog.UI/Controllers/CategoriesController.Create.cs
+++ HotelLandonBlog.UI/Controllers/CategoriesController.Create.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace HotelLandonBlog.UI.Controllers
 {
-    public partial class CategoriesController : GenericController<IRepository<Category>, BlogPost>, IRazorController<Category>
-    {
-        // Get
-        [HttpGet]
-        public override Task<ActionResult<Category>> Create()
-        {
-            return View(default(Category));
-        }
-        // Post
-        [HttpPost("[action]/{id}")]
-        public override Task<ActionResult<Category>> Create([Bind(new[] {
-            nameof(Category.Name),
-            })] Category entity, int id) => base.Create(entity, id);
-    }
+    //public partial class CategoriesController : GenericController<IRepository<Category>, BlogPost>, IRazorController<Category>
+    //{
+    //    // Get
+    //    [HttpGet]
+    //    public override Task<ActionResult<Category>> Create()
+    //    {
+    //        return View(default(Category));
+    //    }
+    //    // Post
+    //    [HttpPost("[action]/{id}")]
+    //    public override Task<ActionResult<Category>> Create([Bind(new[] {
+    //        nameof(Category.Name),
+    //        })] Category entity, int id) => base.Create(entity, id);
+    //}
 }
