f3501b8230657413a14ee767af81a9e752f9bb55
saad mohammad <saadmoh99@hotmail.com>
2022-02-03 08:57:07 +0100

    test

--- HotelLandonBlog.UI/Controllers/BlogPostsController.Edit.cs
+++ HotelLandonBlog.UI/Controllers/BlogPostsController.Edit.cs
@@ -2,21 +2,61 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using HotelLandonBlog.Repository;
+using Microsoft.AspNetCore;
+using System.Web.Mvc;
+using System.Web.Http.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace HotelLandonBlog.UI.Controllers
 {
-    public partial class BlogPostsController
+    public partial class BlogPostsController : GenericController<IRepository<BlogPost>, BlogPost>, IRazorController<BlogPost>
     {
         // Get
-        public Task<ActionResult<BlogPost>> Edit(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        // Post
-        public Task<ActionResult<BlogPost>> Edit(int id, BlogPost t)
+        public override async Task<IActionResult<BlogPost>> Edit(int id)
         {
             throw new NotImplementedException();
         }
     }
+    // Post
+    //public Task<ActionResult<BlogPost>> Edit(int id, BlogPost t)
+
+    //{
+    //    if (!entity.Id.Equals(id))
+    //    {
+    //        return null; //View("NotFound");
+    //    }
+    //    if (ModelState.IsValid)
+    //    {
+    //        if (await this.repository.UpdateAsync(entity, id))
+    //        {
+    //            return RedirectToAction();
+    //        }
+    //    }
+    //    return View(entity);
+    //    // Create
+    //    // Update
+    //}
+
+    [HttpPost("[action]/{id?}")]
+    public override async Task<IActionResult> Edit([Bind(new[] {
+            nameof(BlogPost.Id),
+            nameof(BlogPost.Status),
+            nameof(BlogPost.Category),
+            nameof(BlogPost.Title),
+            nameof(BlogPost.Content),
+            nameof(BlogPost.LastUpdate),})] BlogPost entity, int id)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(entity);
+    }
+  //  => base.Edit(id, entity);
 }
+
