f70727dd49bc031619285321b09f71e868a5fe6f
saad mohammad <saadmoh99@hotmail.com>
2022-02-04 11:02:30 +0100

    edit-blogPost

--- HotelLandonBlog.UI/Controllers/BlogPostsController.Edit.cs
+++ HotelLandonBlog.UI/Controllers/BlogPostsController.Edit.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using HotelLandonBlog.Repository;
 using Microsoft.AspNetCore;
-using System.Web.Mvc;
-using System.Web.Http.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -14,41 +12,20 @@ namespace HotelLandonBlog.UI.Controllers
 {
     public partial class BlogPostsController : GenericController<IRepository<BlogPost>, BlogPost>, IRazorController<BlogPost>
     {
-        // Get
-        public override async Task<IActionResult<BlogPost>> Edit(int id)
-        {
-            if (!entity.Id.Equals(id))
-            {
-                return View("NotFound");
-            }
-            if (ModelState.IsValid)
-            {
-                if (await this.repository.UpdateAsync(entity, id))
-                {
-                    return RedirectToAction("Read", "Home");
-                }
-            }
-        }
-    }
-    
+        
 
-    [HttpPost("[action]/{id?}")]
-    public override async Task<IActionResult> Edit([Bind(new[] {
+        [HttpPost("[action]/{id?}")]
+        public override Task<IActionResult> Edit(int id , [Bind(new[] {
             nameof(BlogPost.Id),
             nameof(BlogPost.Status),
             nameof(BlogPost.Category),
             nameof(BlogPost.Title),
             nameof(BlogPost.Content),
-            nameof(BlogPost.LastUpdate),})] BlogPost entity, int id)=> base.Edit(id, entity);
-    //{
-    //    //if (ModelState.IsValid)
-    //    //{
-    //    //    db.Entry(entity).State = EntityState.Modified;
-    //    //    db.SaveChanges();
-    //    //    return RedirectToAction("Index");
-    //    //}
-    //    //return View(entity);
-    //}
-  //  
+            nameof(BlogPost.LastUpdate),})] BlogPost entity) => base.Edit(id, entity);
+    }
+    
+
+    
+    
 }
 
